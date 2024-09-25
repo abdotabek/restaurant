92612534bafc5818db6a812b3b9e5703fc23333f
otabekAbdulazizov <abdulazizovotabek59@gmail.com>
2024-09-25 17:40:59 +0800

    update

--- src/main/java/com/example/restaurant/controller/MenuController.java
+++ src/main/java/com/example/restaurant/controller/MenuController.java
@@ -1,14 +1,14 @@
 package com.example.restaurant.controller;
 
+import com.example.restaurant.domain.Menu;
 import com.example.restaurant.dto.MenuDto;
 import com.example.restaurant.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
@@ -16,14 +16,36 @@ public class MenuController {
     private final MenuService menuService;
 
     @Autowired
-
     public MenuController(MenuService menuService) {
         this.menuService = menuService;
     }
+
     @PostMapping("/create")
     public ResponseEntity<MenuDto> addMenu(@RequestBody MenuDto menuDto) {
         MenuDto addMenu = menuService.addMenu(menuDto);
         return new ResponseEntity<>(addMenu, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Menu> updateById(@PathVariable("id") Long id, @RequestBody MenuDto menuDto) {
+        Menu menu = menuService.updateMenuById(id, menuDto);
+        menu.setName(menuDto.getName());
+        menu.setDescription(menuDto.getDescription());
+        menu.setCategory(menuDto.getCategory());
+        menu.setPrice(menuDto.getPrice());
+        return ResponseEntity.ok(menu);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Menu>> getAllMenu() {
+        List<Menu> allMenu = menuService.getAllMenu();
+        return ResponseEntity.ok(allMenu);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Menu> deleteById(@PathVariable("id") Long id) {
+        menuService.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/example/restaurant/service/MenuService.java
+++ src/main/java/com/example/restaurant/service/MenuService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 public class MenuService {
@@ -22,4 +24,24 @@ public class MenuService {
         Menu menu = dto.toEntity();
         return MenuDto.fromEntity(menuRepository.save(menu));
     }
+
+    public List<Menu> getAllMenu() {
+        return menuRepository.findAll();
+    }
+
+    @Transactional
+    public Menu updateMenuById(Long id, MenuDto menuDto) {
+        Menu menu = menuRepository.findById(id).orElse(null);
+        menu.setName(menuDto.getName());
+        menu.setCategory(menuDto.getCategory());
+        menu.setDescription(menuDto.getDescription());
+        menu.setPrice(menuDto.getPrice());
+        return menuRepository.save(menu);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        menuRepository.deleteById(id);
+    }
+
 }
